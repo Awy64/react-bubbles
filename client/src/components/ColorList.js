1678a46ca7cae6e21280c78b0b1d6cdc72398865
Adam-Young <awy6448@gmail.com>
2020-06-12 22:29:26 -0400

    created a login page with token ability. created an custom axios hook for auth. created a PrivateRoute that requires a token to access. created a fetch function with axios.get and set the colors to state. created a update function for colors via axios.put. Next need to work on delete function.

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +8,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  // console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,10 +22,20 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        setEditing(false)
+        updateColors()
+      })
+      .catch(err => console.log(err.message))
+    
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth()
+      .delete()
+    
   };
 
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+    const [person, setPerson] = useState({ username: "", password: "" });
+
+    const handleChange = e => {
+      setPerson({
+        ...person,
+        [e.target.name]: e.target.value,
+      })
+    };
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      axiosWithAuth()
+        .post('/login', person)
+        .then(res => {
+          localStorage.setItem('token', res.data.payload)
+          props.history.push('/BubblePage')
+          console.log(res);
+        })
+        .catch(err => console.log(err.message))
+    };
+
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='username'
+          value={person.username}
+          onChange={handleChange}
+          placeholder="username"
+        />
+        <input
+          type="password"
+          name='password'
+          value={person.password}
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <button>Log In</button>
+      </form>
+    </div>
   );
 };
 
